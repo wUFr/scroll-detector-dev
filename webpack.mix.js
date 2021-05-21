5b269bf67ba974275c66dc80c44b5739e4c9e9bc
Jiří Bělský <jiri.belsky@feo.cz>
2021-05-21 13:24:50 +0200

    updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
 const { min } = require('bn.js');
 const mix = require('laravel-mix');
 
+// SCRIPTS
 mix.ts("./ts/scrollDetector.ts",      "./dist/js/scrollDetector.js")
 mix.ts("./ts/scrollDetector.init.ts", "./dist/js/scrollDetector.init.js")
 
-
 // STYLES
 mix.less('./less/scrollDetector.less',     './dist/css/scrollDetector.css')
-mix.less('./less/scrollDetector.demo.less','./dist/css/scrollDetector.demo.css')
+mix.less('./less/scrollDetector.demo.less','./css/scrollDetector.demo.css')
